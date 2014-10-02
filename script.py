ee3229c7f99249e42b963b11ce5a46dcd785fa2d
Juan Shishido <juanshishido@cal.berkeley.edu>
2014-10-01 23:07:48 -0700

    Extracts training titles and URLs from the RSS.mlx page. Then, navigates to each page and pulls the date information. Stores the result in a DataFrame.

--- script.py
+++ script.py
@@ -1,8 +1,46 @@
 import bs4
 import urllib2
+import pandas as pd
 
+# Point to RSS
 rss = "http://dlab.berkeley.edu/training/rss.xml"
 
-page = bs4.BeautifulSoup(urllib2.urlopen("http://dlab.berkeley.edu/training/intro-data-visualization-arcgis-0").read())
+# Pull RSS data
+rss_data = bs4.BeautifulSoup(urllib2.urlopen(rss).read())
 
-print(page.find('span', {"class" : "date-display-single"}))
+# Pull title and url data
+titles = rss_data.find_all('title')
+urls = rss_data.find_all('link')
+
+# Check equality ?
+len(titles)
+len(urls)
+
+# Listify
+t_list = list(titles)
+u_list = list(urls)
+
+# Remove tags
+title_list	= []
+url_list	= []
+for entry in range(0, len(t_list)):
+	title_list.append(str(t_list[entry])[7 : -8])
+	url_list.append(str(u_list[entry])[6 : -7])
+
+# Dictionary (how to ensure 1-to-1 match?)
+rss_dict = {'title' : title_list, 'url' : url_list}
+
+# DataFrame
+df = pd.DataFrame(rss_dict)
+
+# Dates
+df['date'] = ''
+for item in df.url.index:
+	try:
+		event_url = df.url[item]
+		test_page = bs4.BeautifulSoup(urllib2.urlopen(event_url))
+		date_info = test_page.find('span', {'class' : 'date-display-single'})
+		df.date[item] =  date_info.text
+	except:
+		df.date[item] = 'no date'
+# THERE ARE DUPLICATES
